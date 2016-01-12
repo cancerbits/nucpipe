4ff2b0be20195ac2927c526502fc8a05387eb2ce
nsheff <nsheff@users.noreply.github.com>
2016-01-12 22:32:00 +0100

    Add epilog spike-in conversion estimates

--- pipelines/tools/tsv_parser.py
+++ pipelines/tools/tsv_parser.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 """
 This is just a simple parser to grab, from a tsv file, a certain column.
+You can also specify to grab this column only from a certain row, which you can
+key with any number of column key specificities.
 I wrote it to pull output from biseq methcalling results, but it could
-be used for anything... just give a tsv file and a key column to pull.
+be used for anything... just give a tsv file and a key column to pull, and any
+rows to restrict to if desired.
 """
 
 __author__ = "Nathan C. Sheffield"
@@ -23,20 +26,38 @@ parser.add_argument('-c', '--col', dest='column',
 	help="Choose column(s) to extract",
 	required=True)
 
+parser.add_argument('-r', '--rowmatch', dest='rowmatch',
+	default="", nargs="+", type=str,
+	help="Choose row(s) to extract; format: key=value. Multiple, space-separated
+	key=value pairs are accepted.",
+	required=False)
+
 parser.add_argument('-k', '--keys', dest='include_keys',
  	default=False, action="store_true",
 	help="Print keys?",
 	required=False)
-
-
 
 args = parser.parse_args()
 
 input_open = open(args.input_file, 'rb')
 f = csv.DictReader(input_open, delimiter="\t")
 
+fail = False
+
 for row in f:  # iterates the rows of the file in order
 	#print(row)
+	if args.rowmatch:
+		fail = False
+		for rowmatch_string in args.rowmatch:
+			rowmatch_list = rowmatch_string.split("=")
+			# print(rs)
+			# rowmatch_list[0]  # key
+			# rowmatch_list[1]  # value
+			if not row[rowmatch_list[0]] == rowmatch_list[1]:
+				fail = True
+	if fail:
+		continue
+
 	for key in args.column:
 		if args.include_keys:
 			print("\t".join([key, row[key]]))
